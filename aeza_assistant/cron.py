21772646223552ecabcd6e60add2f1eaffb14f0e
cofob <cofob@riseup.net>
2023-02-24 03:28:56 +0300

    Fix notifications

--- aeza_assistant/cron.py
+++ aeza_assistant/cron.py
@@ -37,7 +37,7 @@ class Cron:
         self.queue = queue
         self.bot_state = bot_state
         self.prev_statuses: dict[str, tuple[bool, float]] = {}
-        self.notified_statuses: dict[str, bool] = {}
+        self.last_notified: dict[str, bool] = {}
         self.push_addresses = push_addresses
         self.session = session
 
@@ -53,18 +53,18 @@ class Cron:
             log.info("Cron job finished")
             await sleep(self.interval)
 
-    async def send_notification(
-        self, session: AsyncSession, name: str, status: bool
-    ) -> None:
+    async def send_notification(self, session: AsyncSession, statuses: dict[str, bool]) -> None:
         chats = await ChatModel.get_list_by_key(session, ChatModel.is_subscribed, True)
         for chat in chats:
             self.queue.put_nowait(
                 self.bot.send_message(
                     chat.telegram_id,
                     Texts.state_changed.format(
-                        Texts.available.format(name)
-                        if status
-                        else Texts.unavailable.format(name)
+                        ",\n".join(
+                            Texts.available.format(name) if status else Texts.unavailable.format(name)
+                            for name, status in statuses.items()
+                        )
+                        + "."
                     ),
                 )
             )
@@ -73,9 +73,7 @@ class Cron:
         async with self.session.get(url) as resp:
             log.debug(f"Push notification for {url} sent")
             if resp.status != 200:
-                log.error(
-                    f"Push notification for {url} failed with {resp.status}: {await resp.text()}"
-                )
+                log.error(f"Push notification for {url} failed with {resp.status}: {await resp.text()}")
 
     async def job(self, session: AsyncSession) -> None:
         statuses = await self.api.get_product_group_statuses()
@@ -94,6 +92,10 @@ class Cron:
         changed_statuses: list[str] = []
 
         for name in readable_statuses:
+            if name not in self.last_notified:
+                self.last_notified[name] = readable_statuses[name]
+
+        for name in readable_statuses:
             if name not in self.prev_statuses:
                 log.debug(f"New tariff group: {name}")
                 self.prev_statuses[name] = (
@@ -101,22 +103,14 @@ class Cron:
                     curr_time,
                 )
                 changed_statuses.append(name)
-            elif (
-                name in self.prev_statuses
-                and self.prev_statuses[name][0] == readable_statuses[name]
-            ):
+            elif name in self.prev_statuses and self.prev_statuses[name][0] == readable_statuses[name]:
                 log.debug(f"Tariff group {name} is still {readable_statuses[name]}")
                 curr_statuses[name] = (
                     readable_statuses[name],
                     self.prev_statuses[name][1],
                 )
-            elif (
-                name in self.prev_statuses
-                and self.prev_statuses[name][0] != readable_statuses[name]
-            ):
-                log.info(
-                    f"Tariff group {name} changed from {self.prev_statuses[name][0]} to {readable_statuses[name]}"
-                )
+            elif name in self.prev_statuses and self.prev_statuses[name][0] != readable_statuses[name]:
+                log.info(f"Tariff group {name} changed from {self.prev_statuses[name][0]} to {readable_statuses[name]}")
                 curr_statuses[name] = (
                     readable_statuses[name],
                     curr_time,
@@ -128,14 +122,13 @@ class Cron:
             if name in self.push_addresses and self.prev_statuses[name][0]:
                 create_task(self.send_push(self.push_addresses[name]))
 
+        changed = {}
+
         for name in self.prev_statuses:
             status, last_change = self.prev_statuses[name]
-            if (
-                curr_time - last_change >= self.notify_sleep
-                and self.notified_statuses.get(name, status) != status
-            ):
-                self.notified_statuses[name] = status
-                log.info(
-                    "Tariff is changed state for more than 1 hour, sending notification"
-                )
-                await self.send_notification(session, name, status)
+            if curr_time - last_change >= self.notify_sleep and self.last_notified.get(name, status) is not status:
+                self.last_notified[name] = status
+                changed[name] = status
+
+        if changed:
+            await self.send_notification(session, changed)
