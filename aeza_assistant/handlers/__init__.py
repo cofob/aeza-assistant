b36ad6289c76212ede824b66807e4cb464e5e8d2
cofob <cofob@riseup.net>
2023-02-24 12:31:51 +0300

    Add added to chat notification

--- aeza_assistant/handlers/__init__.py
+++ aeza_assistant/handlers/__init__.py
@@ -1,1 +1,1 @@
-from . import start, status, subscribe_callbacks
+from . import added_to_group, start, status, subscribe_callbacks
---
+++ aeza_assistant/handlers/added_to_group.py
@@ -0,0 +1,26 @@
+from logging import getLogger
+
+from aiogram import Bot
+from aiogram.enums import ChatMemberStatus
+from aiogram.types import ChatMemberUpdated
+
+from ..texts import Texts
+from .router import router
+
+log = getLogger(__name__)
+
+
+@router.my_chat_member()
+async def on_chat_member_updated(update: ChatMemberUpdated, bot: Bot) -> None:
+    if update.chat.type == "private":
+        return
+    if (
+        update.new_chat_member.status == ChatMemberStatus.MEMBER
+        and update.old_chat_member.status == ChatMemberStatus.LEFT
+        and update.new_chat_member.user.id == bot.id
+    ):
+        log.info(f"Bot was added to chat {update.chat.id}")
+        await bot.send_message(
+            update.chat.id,
+            Texts.added_to_chat.format(username=update.new_chat_member.user.username),
+        )
